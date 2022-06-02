0a1019808ad857b5577ee880fc09d7071436d5de
Mara Kim <mara@autochthe.net>
2022-06-02 02:59:30 -0700

    Reorganize

---
+++ bin/kismet_bot/src/cli.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::io::Write;
+
+pub fn run() {
+    println!(
+        "\
+        Hello, I am Kismet <3\n\
+        Input a roll and press ENTER.\n\
+        Exit with 'exit'.\n\
+        "
+    );
+
+    loop {
+        print!("> ");
+        if let Err(error) = io::stdout().flush() {
+            panic!("{}", error)
+        }
+
+        let mut raw_input = String::new();
+        io::stdin()
+            .read_line(&mut raw_input)
+            .expect("Failed to read line");
+        let input = raw_input.trim();
+        if input == "exit" {
+            println!("Goodbye <3");
+            break;
+        } else {
+            println!("{}", input)
+        }
+    }
+}
--- bin/kismet_bot/src/main.rs
+++ bin/kismet_bot/src/main.rs
@@ -1,31 +1,5 @@
-use std::io;
-use std::io::Write;
+mod cli;
 
 fn main() {
-    println!(
-        "\
-        Hello, I am Kismet <3\n\
-        Input a roll and press ENTER.\n\
-        Exit with 'exit' or CTRL-D.\n\
-        "
-    );
-
-    loop {
-        print!("> ");
-        if let Err(error) = io::stdout().flush() {
-            panic!("{}", error)
-        }
-
-        let mut raw_input = String::new();
-        io::stdin()
-            .read_line(&mut raw_input)
-            .expect("Failed to read line");
-        let input = raw_input.trim();
-        if input == "exit" {
-            println!("Goodbye <3");
-            break;
-        } else {
-            println!("{}", input)
-        }
-    }
+    cli::run();
 }
