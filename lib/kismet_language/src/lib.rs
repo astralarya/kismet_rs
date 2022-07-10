43c1b518c3ccfe544adc3d3806db080e49030b5d
Mara Kim <mara@autochthe.net>
2022-07-10 05:52:57 -0700

    Use type alias

--- lib/kismet_language/src/lib.rs
+++ lib/kismet_language/src/lib.rs
@@ -7,8 +7,8 @@ extern crate lalrpop_util;
 
 lalrpop_mod!(pub kismet);
 
-pub fn parse<'input>(
-    input: &'input String,
-) -> Result<ast::Expr, ParseError<usize, Token<'input>, &'input str>> {
+type ParseResult<'life> = Result<ast::Expr, ParseError<usize, Token<'life>, &'life str>>;
+
+pub fn parse<'input>(input: &'input String) -> ParseResult<'input> {
     kismet::KismetParser::new().parse(&input)
 }
