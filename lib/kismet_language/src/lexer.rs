05c3684579a37900c28b6b6cd962b036a616ff89
Mara Kim <mara@autochthe.net>
2022-07-11 13:53:37 -0700

    Id parsing

--- lib/kismet_language/src/lexer.rs
+++ lib/kismet_language/src/lexer.rs
@@ -64,7 +64,7 @@ pub enum Token<'input> {
     #[regex(r"[0-9]+", parse_int)]
     Int(i32),
 
-    #[regex(r"\$[_a-zA-Z][_a-zA-Z0-9]*")]
+    #[regex(r"([_a-ce-zA-CE-Z]|d[_a-zA-Z])[_a-zA-Z0-9]*", priority = 0)]
     Id(&'input str),
 }
 
