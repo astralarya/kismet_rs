9990b20ea20bbad54609c32c0a2f493c1287260a
Mara Kim <mara@autochthe.net>
2022-07-11 19:11:40 -0700

    Fix parens

--- lib/kismet_language/src/ast.rs
+++ lib/kismet_language/src/ast.rs
@@ -20,6 +20,15 @@ pub enum Node<'input> {
     Error(Box<dyn Error>),
 }
 
+impl<'input> Node<'input> {
+    fn is_int(&self) -> bool {
+        match self {
+            Node::Int(_) => true,
+            _ => false,
+        }
+    }
+}
+
 impl fmt::Display for Node<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fn fmt_seq(f: &mut fmt::Formatter, nodes: &Vec<Node>, delim: &'static str) -> fmt::Result {
@@ -36,10 +45,19 @@ impl fmt::Display for Node<'_> {
 
         match self {
             Node::Stmts(nodes) => fmt_seq(f, nodes, "\n"),
-            Node::Exprs(nodes) => fmt_seq(f, nodes, ", "),
-            Node::Op(left, op, right) => {
-                write!(f, "{}{}{}{}{}", left, op.space(), op, op.space(), right)
+            Node::Exprs(nodes) => {
+                fmt_seq(f, nodes, ", ")?;
+                match nodes.len() {
+                    1 => write!(f, ","),
+                    _ => Ok(()),
+                }
             }
+            Node::Op(left, op, right) => match (left.is_int(), op) {
+                (false, Token::DIE) => {
+                    write!(f, "({}){}{}{}{}", left, op.space(), op, op.space(), right)
+                }
+                _ => write!(f, "{}{}{}{}{}", left, op.space(), op, op.space(), right),
+            },
             Node::Unary(op, right) => {
                 write!(f, "{}{}{}", op, op.space(), right)
             }
