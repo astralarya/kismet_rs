16cd308626edb9968ca6313cbd4b8339c618453e
Mara Kim <mara@autochthe.net>
2022-07-18 06:27:39 -0700

    Fix nodes

--- lib/kismet_language/src/ast/primary.rs
+++ lib/kismet_language/src/ast/primary.rs
@@ -4,7 +4,7 @@ use super::{Atom, Node};
 
 #[derive(Debug, PartialEq)]
 pub enum Primary<'input> {
-    Die(Node<Primary<'input>>),
+    Die(Node<Atom<'input>>),
     Attribute(Node<Primary<'input>>, Node<&'input str>),
     Atom(Atom<'input>),
 }
@@ -13,9 +13,7 @@ impl fmt::Display for Primary<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Primary::Die(val) => match *val.kind {
-                Primary::Atom(Atom::Integer(_))
-                | Primary::Atom(Atom::Tuple(_))
-                | Primary::Atom(Atom::ListDisplay(_)) => {
+                Atom::Integer(_) | Atom::Tuple(_) | Atom::ListDisplay(_) => {
                     write!(f, "d{}", val)
                 }
                 _ => write!(f, "d({})", val),
