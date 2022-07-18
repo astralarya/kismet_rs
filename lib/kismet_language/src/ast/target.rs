31a477aba29a4ee14ab808f9a8b938e30d629669
Mara Kim <mara@autochthe.net>
2022-07-18 04:39:16 -0700

    destructure objects

--- lib/kismet_language/src/ast/target.rs
+++ lib/kismet_language/src/ast/target.rs
@@ -7,6 +7,16 @@ pub enum Target<'input> {
     Id(&'input str),
     TargetTuple(Vec<Node<Target<'input>>>),
     TargetList(Vec<Node<Target<'input>>>),
+    TargetDict(Vec<Node<TargetDictItem<'input>>>),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TargetDictItem<'input> {
+    Shorthand(&'input str),
+    Pair {
+        key: Node<&'input str>,
+        val: Node<Target<'input>>,
+    },
 }
 
 impl fmt::Display for Target<'_> {
@@ -15,6 +25,16 @@ impl fmt::Display for Target<'_> {
             Target::Id(val) => write!(f, "{}", val),
             Target::TargetTuple(val) => write!(f, "({})", Node::vec_to_string(&val, ", ")),
             Target::TargetList(val) => write!(f, "[{}]", Node::vec_to_string(&val, ", ")),
+            Target::TargetDict(val) => write!(f, "{{{}}}", Node::vec_to_string(&val, ", ")),
+        }
+    }
+}
+
+impl fmt::Display for TargetDictItem<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TargetDictItem::Shorthand(val) => write!(f, "{}", val),
+            TargetDictItem::Pair { key, val } => write!(f, "{}: {}", key, val),
         }
     }
 }
