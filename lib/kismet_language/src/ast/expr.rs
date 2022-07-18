0eb67a0f12029ff1b722d2e7bc28850ab4c61827
Mara Kim <mara@autochthe.net>
2022-07-18 06:41:26 -0700

    Die back in Expr

--- lib/kismet_language/src/ast/expr.rs
+++ lib/kismet_language/src/ast/expr.rs
@@ -10,6 +10,7 @@ pub enum Expr<'input> {
     Op(Node<Expr<'input>>, Token<'input>, Node<Expr<'input>>),
     Unary(Token<'input>, Node<Expr<'input>>),
     Coefficient(Node<Atom<'input>>, Node<Expr<'input>>),
+    Die(Node<Atom<'input>>),
     Primary(Primary<'input>),
 }
 
@@ -22,6 +23,10 @@ impl fmt::Display for Expr<'_> {
             }
             Expr::Unary(lhs, val) => write!(f, "{}{}{}", lhs, lhs.space(), val),
             Expr::Coefficient(lhs, rhs) => write!(f, "{}{}", lhs, rhs),
+            Expr::Die(val) => match *val.kind {
+                Atom::Id(_) => write!(f, "d({})", val),
+                _ => write!(f, "d{}", val),
+            },
             Expr::Primary(val) => write!(f, "{}", val),
         }
     }
--- lib/kismet_language/src/ast/primary.rs
+++ lib/kismet_language/src/ast/primary.rs
@@ -4,7 +4,6 @@ use super::{Atom, Node};
 
 #[derive(Debug, PartialEq)]
 pub enum Primary<'input> {
-    Die(Node<Atom<'input>>),
     Attribute(Node<Primary<'input>>, Node<&'input str>),
     Atom(Atom<'input>),
 }
@@ -12,10 +11,6 @@ pub enum Primary<'input> {
 impl fmt::Display for Primary<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Primary::Die(val) => match *val.kind {
-                Atom::Id(_) => write!(f, "d({})", val),
-                _ => write!(f, "d{}", val),
-            },
             Primary::Attribute(lhs, rhs) => write!(f, "{}.{}", lhs, rhs),
             Primary::Atom(val) => write!(f, "{}", val),
         }
