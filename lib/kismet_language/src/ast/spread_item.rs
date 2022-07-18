b6c5d57f1e4990112e8272767ee5577cbd31d8f4
Mara Kim <mara@autochthe.net>
2022-07-18 01:47:41 -0700

    Refactor

--- lib/kismet_language/src/ast/spread_item.rs
+++ lib/kismet_language/src/ast/spread_item.rs
@@ -1,22 +1,11 @@
 use std::fmt;
 
-use crate::types::Span;
-
 use crate::ast::{Expr, Node};
 
 #[derive(Debug, PartialEq)]
 pub enum SpreadItem<'input> {
     Expr(Expr<'input>),
     Spread(Node<Expr<'input>>),
-}
-
-impl<'input> Node<SpreadItem<'input>> {
-    pub fn spread_item((span, value): (Span, SpreadItem<'input>)) -> Node<SpreadItem<'input>> {
-        Node {
-            span,
-            kind: Box::new(value),
-        }
-    }
 }
 
 impl fmt::Display for SpreadItem<'_> {
