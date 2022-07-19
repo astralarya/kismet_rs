25f8f5758e86d5d6c9c125b766a93022fe47cd0a
Mara Kim <mara@autochthe.net>
2022-07-19 08:32:50 -0700

    Parser runner

--- lib/kismet_language/src/parser/mod.rs
+++ lib/kismet_language/src/parser/mod.rs
@@ -1,14 +1,23 @@
 pub mod token;
 
-use nom::IResult;
+use nom::{error::Error, Err, Parser};
 
 use token::Token;
 
 use crate::{ast::Node, types::Span};
 
-pub struct ParseError {}
-//type ParseResult<'input> = Result<Node<Expr<'input>>, ParseError>;
+type ParseResult<I, O> = Result<Node<O>, Err<Error<Node<I>>>>;
 
-pub fn parse<'input>(input: &'input str) -> IResult<Node<&'input str>, Node<Token<'input>>> {
-    token::delim(Node::new(Span::from(input), input))
+pub fn parse<'input>(input: &'input str) -> ParseResult<&'input str, Token<'input>> {
+    run_parser(&mut token::delim, input)
+}
+
+pub fn run_parser<I, O, P>(parser: &mut P, input: I) -> ParseResult<I, O>
+where
+    P: Parser<Node<I>, Node<O>, Error<Node<I>>>,
+    Span: From<I>,
+    I: Copy,
+{
+    let (_, result) = parser.parse(Node::new(Span::from(input), input))?;
+    Ok(result)
 }
