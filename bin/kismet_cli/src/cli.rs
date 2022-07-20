a61d8759cc561b466d0d9389ca3d2735e36266aa
Mara Kim <mara@autochthe.net>
2022-07-20 15:18:15 -0700

    Bring back display

--- bin/kismet_cli/src/cli.rs
+++ bin/kismet_cli/src/cli.rs
@@ -35,9 +35,8 @@ pub fn run(state: &mut State) {
                 } else {
                     match parse(&line) {
                         Ok(x) => match state.print {
-                            PrintLevel::Debug | PrintLevel::Output => println!("{:#?}", x),
-                            // PrintLevel::Debug => println!("{:#?}\n{}", x, x),
-                            // PrintLevel::Output => println!("{}", x),
+                            PrintLevel::Debug => println!("{:#?}\n{}", x, x),
+                            PrintLevel::Output => println!("{}", x),
                             PrintLevel::None => (),
                         },
                         Err(e) => eprintln!("{:#?}", e),
