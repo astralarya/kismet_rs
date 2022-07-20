35bec3e62db757c9c914461a2291d04d7d58e6ab
Mara Kim <mara@autochthe.net>
2022-07-20 12:46:29 -0700

    Strings

--- lib/kismet_language/src/parser/atom.rs
+++ lib/kismet_language/src/parser/atom.rs
@@ -5,6 +5,15 @@ use super::{token_action, KResult, NumberKind, Token};
 // pub fn atom<'input>(input: Node<&'input str>) -> KResult<Node<&'input str>, Node<Atom<'input>>> {
 // }
 
+pub fn string_literal<'input>(
+    input: Node<&'input str>,
+) -> KResult<Node<&'input str>, Node<Atom<'input>>> {
+    token_action(|x| match *x.data {
+        Token::String(y) => Some(Node::new(x.span, Atom::String(y))),
+        _ => None,
+    })(input)
+}
+
 pub fn numeric_literal<'input>(
     input: Node<&'input str>,
 ) -> KResult<Node<&'input str>, Node<Atom<'input>>> {
