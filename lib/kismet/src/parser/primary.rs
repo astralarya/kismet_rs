28f7814d63a4e086af50d504e144cb3ada404543
Mara Kim <mara@autochthe.net>
2022-07-26 00:36:27 -0700

    attribute

--- lib/kismet/src/parser/primary.rs
+++ lib/kismet/src/parser/primary.rs
@@ -1,11 +1,39 @@
-use nom::branch::alt;
+use nom::{combinator::opt, sequence::preceded};
 
 use crate::{ast::Primary, types::Node};
 
-use super::{atom, Input, KResult};
+use super::{atom, token_tag, token_tag_id, Input, KResult, Token};
 
 pub fn primary<'input>(i: Input<'input>) -> KResult<'input, Node<Primary>> {
-    primary_node(i)
+    let (mut i, mut iter) = primary_node(i)?;
+    let mut prev = i.len();
+    loop {
+        (i, iter) = primary_iter(iter)(i)?;
+        if i.len() == prev {
+            break;
+        } else {
+            prev = i.len()
+        }
+    }
+    Ok((i, iter))
+}
+
+pub fn primary_iter<'input>(
+    iter: Node<Primary>,
+) -> impl Fn(Input<'input>) -> KResult<'input, Node<Primary>> {
+    move |i| {
+        let (i, val) = opt(preceded(token_tag(Token::DOT), token_tag_id))(i)?;
+        match val {
+            Some(val) => Ok((
+                i,
+                Node::new(
+                    iter.span.clone() + val.span.clone(),
+                    Primary::Attribute(iter.clone(), val),
+                ),
+            )),
+            None => Ok((i, iter.clone())),
+        }
+    }
 }
 
 pub fn primary_node<'input>(i: Input<'input>) -> KResult<'input, Node<Primary>> {
