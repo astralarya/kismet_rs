8d74ffaedb4e1e98657a24447c67a67953f69149
Mara Kim <mara@autochthe.net>
2022-07-28 16:54:51 -0700

    fix errors

--- lib/kismet/src/parser/stmt.rs
+++ lib/kismet/src/parser/stmt.rs
@@ -1,20 +1,33 @@
 use nom::{
     combinator::all_consuming,
     multi::{many0, many1, separated_list0},
+    Err,
 };
 
 use crate::{
     ast::Expr,
-    types::{Node, Span},
+    types::{Node, ONode, Span},
 };
 
-use super::{expr, token_tag, Input, KResult, Token};
+use super::{expr, token_tag, ErrorKind, Input, KResult, Token};
 
-pub fn expr_list0<'input>(i: Input<'input>) -> KResult<'input, Node<Vec<Node<Expr>>>> {
+pub fn expr_list0<'input>(i: Input<'input>) -> KResult<'input, Option<Node<Vec<Node<Expr>>>>> {
+    let i_span = match Span::get0(i) {
+        Some(x) => x,
+        None => return Ok((i, None)),
+    };
     let (i, _lhs) = many0(token_tag(Token::DELIM))(i)?;
     let (i, val) = separated_list0(many1(token_tag(Token::DELIM)), expr)(i)?;
     let (i, _rhs) = many0(token_tag(Token::DELIM))(i)?;
-    Ok((i, Node::new(Span::reduce_ok(&val)?, val)))
+    Ok((
+        i,
+        Some(Node::new(
+            Span::reduce(&val).unwrap_or(Span::new(
+                i_span.start..Span::get0(i).map(|x| x.start).unwrap_or(i_span.end),
+            )),
+            val,
+        )),
+    ))
 }
 
 pub fn expr_list1<'input>(i: Input<'input>) -> KResult<'input, Node<Vec<Node<Expr>>>> {
@@ -35,6 +48,13 @@ pub fn expr_list1<'input>(i: Input<'input>) -> KResult<'input, Node<Vec<Node<Exp
 }
 
 pub fn start<'input>(i: Input<'input>) -> KResult<'input, Node<Expr>> {
+    let i_span = match Span::get0(i) {
+        Some(x) => x,
+        None => return Err(Err::Failure(ONode::new(None, ErrorKind::Eof))),
+    };
     let (i, val) = all_consuming(expr_list0)(i)?;
-    Ok((i, Node::new(val.span, Expr::Stmts(*val.data))))
+    match val {
+        Some(val) => Ok((i, Node::new(val.span, Expr::Stmts(*val.data)))),
+        None => Err(Err::Failure(ONode::new(i_span, ErrorKind::Eof))),
+    }
 }
