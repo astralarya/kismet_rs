9d2bb232f6f6de223e40f1d988d723f95fe5956c
Mara Kim <mara@autochthe.net>
2022-07-31 03:26:14 -0700

    update

--- lib/kismet/src/parser/error.rs
+++ lib/kismet/src/parser/error.rs
@@ -1,6 +1,9 @@
 use nom::{error::ParseError, Needed};
 
-use crate::types::{ONode, Span};
+use crate::{
+    ast::TargetExpr,
+    types::{Node, ONode, Span},
+};
 
 use super::{Error, Input};
 
@@ -14,19 +17,22 @@ pub enum ErrorKind {
     Predicate,
     Grammar,
     Chain(ONode<ErrorKind>, Box<ErrorKind>),
+    Convert(Node<ConvertKind>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ConvertKind {
+    TargetExpr(TargetExpr),
 }
 
 impl<'input> ParseError<Input<'input>> for Error {
     fn from_error_kind(input: Input<'input>, kind: nom::error::ErrorKind) -> Self {
-        ONode::new(
-            input.get(0).map(|x| Span::from(x.span)),
-            ErrorKind::Nom(kind),
-        )
+        ONode::new(Span::get0(input), ErrorKind::Nom(kind))
     }
 
     fn append(input: Input<'input>, kind: nom::error::ErrorKind, other: Self) -> Self {
         ONode::new(
-            input.get(0).map(|x| Span::from(x.span)),
+            Span::get0(input),
             ErrorKind::Chain(other, Box::new(ErrorKind::Nom(kind))),
         )
     }
