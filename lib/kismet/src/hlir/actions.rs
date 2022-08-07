f6875c1d466952f0ed4fcffa2cd848a8f353cad2
Mara Kim <mara@autochthe.net>
2022-08-06 22:57:04 -0700

    rearrange

--- lib/kismet/src/hlir/actions.rs
+++ lib/kismet/src/hlir/actions.rs
@@ -1,8 +1,10 @@
-use super::{BasicBlock, Error, Exec, Instruction, Value};
+use crate::types::Node;
+
+use super::{BasicBlock, Error, Exec, Instruction, SymbolTable, SymbolTableResult, Value};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ValueAction {
-    Noop,
+    ListItemUnpack,
 }
 
 pub type VInstruction = Instruction<ValueAction, Value, Value>;
@@ -11,5 +13,32 @@ pub type VBasicBlock = BasicBlock<ValueAction, Value, Value>;
 impl Exec<Vec<Value>, Value, Error> for ValueAction {
     fn exec(&self, _: Vec<Value>) -> Result<Value, Error> {
         Ok(Value::default())
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Action<T, U, V> {
+    pub args: Vec<Node<Instruction<T, U, V>>>,
+    pub kind: T,
+}
+
+impl<T, U, V, E> Exec<SymbolTable<U>, (SymbolTable<U>, V), Error> for Action<T, U, V>
+where
+    T: Exec<Vec<V>, V, Error>,
+    U: TryFrom<V, Error = E> + Clone + Default,
+    V: From<U> + Clone + Default,
+    Error: From<E>,
+{
+    fn exec(&self, i: SymbolTable<U>) -> SymbolTableResult<U, V> {
+        let (i, args) =
+            self.args
+                .iter()
+                .fold::<Result<_, Error>, _>(Ok((i, vec![])), |acc, val| {
+                    let (i, mut vec) = acc?;
+                    let (i, val) = val.exec(i)?;
+                    vec.push(val);
+                    Ok((i, vec))
+                })?;
+        Ok((i, self.kind.exec(args)?))
     }
 }
--- lib/kismet/src/hlir/program.rs
+++ lib/kismet/src/hlir/program.rs
@@ -2,7 +2,7 @@ use std::slice::Iter;
 
 use crate::{ast::Id, types::Node};
 
-use super::{Error, Exec, SymbolTable, SymbolTableResult};
+use super::{Action, Error, Exec, SymbolTable, SymbolTableResult};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct BasicBlock<T, U, V>(pub Vec<Node<Instruction<T, U, V>>>);
@@ -15,33 +15,6 @@ pub enum Instruction<T, U, V> {
     Block(BasicBlock<T, U, V>),
     Assign(Id, Node<Instruction<T, U, V>>),
     Symbol(U),
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct Action<T, U, V> {
-    pub args: Vec<Node<Instruction<T, U, V>>>,
-    pub kind: T,
-}
-
-impl<T, U, V, E> Exec<SymbolTable<U>, (SymbolTable<U>, V), Error> for Action<T, U, V>
-where
-    T: Exec<Vec<V>, V, Error>,
-    U: TryFrom<V, Error = E> + Clone + Default,
-    V: From<U> + Clone + Default,
-    Error: From<E>,
-{
-    fn exec(&self, i: SymbolTable<U>) -> SymbolTableResult<U, V> {
-        let (i, args) =
-            self.args
-                .iter()
-                .fold::<Result<_, Error>, _>(Ok((i, vec![])), |acc, val| {
-                    let (i, mut vec) = acc?;
-                    let (i, val) = val.exec(i)?;
-                    vec.push(val);
-                    Ok((i, vec))
-                })?;
-        Ok((i, self.kind.exec(args)?))
-    }
 }
 
 impl<T, U, V, E> Exec<SymbolTable<U>, (SymbolTable<U>, V), Error> for Instruction<T, U, V>
