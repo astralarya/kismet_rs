fbb97a6f7415fb043a5a106a553dc3ec5d572864
Mara Kim <mara@autochthe.net>
2022-08-06 20:12:19 -0700

    style

--- lib/kismet/tests/util/mod.rs
+++ lib/kismet/tests/util/mod.rs
@@ -15,36 +15,36 @@ pub fn assert_stmt(node: Node<Expr>, input: &str) {
 }
 
 #[allow(dead_code)]
-pub fn new_arith<'input>(lhs: Node<Expr>, op: Node<OpArith>, rhs: Node<Expr>) -> Node<Expr> {
+pub fn new_arith(lhs: Node<Expr>, op: Node<OpArith>, rhs: Node<Expr>) -> Node<Expr> {
     Node::new(lhs.span + rhs.span, Expr::Op(Op::Arith(lhs, op, rhs)))
 }
 
 #[allow(dead_code)]
-pub fn new_unary<'input>(op: Node<OpArith>, val: Node<Expr>) -> Node<Expr> {
+pub fn new_unary(op: Node<OpArith>, val: Node<Expr>) -> Node<Expr> {
     Node::new(op.span + val.span, Expr::Op(Op::Unary(op, val)))
 }
 
 #[allow(dead_code)]
-pub fn new_op<'input>(range: Range<usize>, op: OpArith) -> Node<OpArith> {
+pub fn new_op(range: Range<usize>, op: OpArith) -> Node<OpArith> {
     Node::new(range, op)
 }
 
 #[allow(dead_code)]
-pub fn new_atom<'input>(range: Range<usize>, val: Atom) -> Node<Expr> {
+pub fn new_atom(range: Range<usize>, val: Atom) -> Node<Expr> {
     Node::new(range, Expr::Primary(Primary::Atom(val)))
 }
 
 #[allow(dead_code)]
-pub fn new_integer<'input>(range: Range<usize>, val: Integer) -> Node<Expr> {
+pub fn new_integer(range: Range<usize>, val: Integer) -> Node<Expr> {
     new_atom(range, Atom::Integer(val))
 }
 
 #[allow(dead_code)]
-pub fn new_string<'input>(range: Range<usize>, val: &'input str) -> Node<Expr> {
+pub fn new_string(range: Range<usize>, val: &str) -> Node<Expr> {
     new_atom(range, Atom::String(String::from(val)))
 }
 
 #[allow(dead_code)]
-pub fn new_id<'input>(range: Range<usize>, val: &'input str) -> Node<Expr> {
+pub fn new_id(range: Range<usize>, val: &str) -> Node<Expr> {
     new_atom(range, Atom::Id(String::from(val)))
 }
